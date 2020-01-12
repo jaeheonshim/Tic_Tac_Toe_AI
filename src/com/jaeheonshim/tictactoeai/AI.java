182ed07395d485a956b3cfd70f879f3b73e20e10
Jaeheon Shim <git@jaeheonshim.com>
2020-01-11 19:23:04 -0500

    Implement MiniMax AI Algorithm

--- src/com/jaeheonshim/tictactoeai/AI.java
+++ src/com/jaeheonshim/tictactoeai/AI.java
@@ -1,14 +1,97 @@
 package com.jaeheonshim.tictactoeai;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class AI {
-    public static int[] getEmptySpot(Board board) {
-        for(int i = 0; i < board.getBoard().length; i++) {
-            for(int j = 0; j < board.getBoard()[i].length; j++) {
-                if(board.getBoard()[i][j] == Board.CellState.BLANK) {
-                    return new int[] {i, j};
-                }
+    public static Board.CellState ai = Board.CellState.X;
+    public static Board.CellState human = Board.CellState.O;
+
+    public static Integer[] getBestMove(Board board) {
+        Integer[] bestMove = null;
+        int bestScore = -100;
+        for (Integer[] position : board.getAvailablePositions()) {
+            board.place(position, ai);
+            int score = minimax(board, false);
+            board.place(position, Board.CellState.BLANK);
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = position;
             }
         }
-        return null;
+        return bestMove;
+    }
+
+    public static int minimax(Board board, boolean maximising) {
+        Board.CellState winner = board.checkWon();
+        int score;
+        if (winner != null) {
+            if (winner == ai) {
+                score = 1;
+                return score;
+            } else if (winner == Board.CellState.BLANK) {
+                score = 0;
+                return score;
+            } else {
+                score = -1;
+                return score;
+            }
+        }
+        if (maximising) {
+            int bestScore = -100;
+            for (Integer[] position : board.getAvailablePositions()) {
+                board.place(position, ai);
+                int currentScore = minimax(board, false);
+                board.place(position, Board.CellState.BLANK);
+                if(currentScore > bestScore) {
+                    bestScore = currentScore;
+                }
+            }
+            return bestScore;
+        } else {
+            int bestScore = 100;
+            for (Integer[] position : board.getAvailablePositions()) {
+                board.place(position, human);
+                int currentScore = minimax(board, true);
+                board.place(position, Board.CellState.BLANK);
+                if(currentScore < bestScore) {
+                    bestScore = currentScore;
+                }
+            }
+            return bestScore;
+        }
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        Scanner in = new Scanner(System.in);
+        board.printBoard();
+        while(board.checkWon() == null) {
+            System.out.print("Enter position (x, y): ");
+            String[] position = in.nextLine().split(",");
+            Integer[] pos = new Integer[2];
+            pos[0] = 2 - Integer.parseInt(position[1].trim());
+            pos[1] = Integer.parseInt(position[0].trim());
+            board.place(pos, Board.CellState.O);
+            board.printBoard();
+            System.out.println();
+            if(board.checkWon() != null) {
+                break;
+            }
+            System.out.println("AI is playing...");
+            board.place(getBestMove(board), Board.CellState.X);
+            board.printBoard();
+        }
+
+        System.out.println("=======================");
+        System.out.println("Game Finished!");
+        board.printBoard();
+        if(board.checkWon() == ai) {
+            System.out.println("AI WON!");
+        } else if(board.checkWon() == human) {
+            System.out.println("YOU WON?!");
+        } else {
+            System.out.println("TIE");
+        }
     }
 }
--- src/com/jaeheonshim/tictactoeai/Board.java
+++ src/com/jaeheonshim/tictactoeai/Board.java
@@ -1,9 +1,12 @@
 package com.jaeheonshim.tictactoeai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
-    private CellState[][] board = new CellState[][] {
+    private CellState[][] board = new CellState[][]{
             {CellState.BLANK, CellState.BLANK, CellState.BLANK},
-            {CellState.BLANK,CellState.BLANK, CellState.BLANK},
+            {CellState.BLANK, CellState.BLANK, CellState.BLANK},
             {CellState.BLANK, CellState.BLANK, CellState.BLANK}
     };
 
@@ -11,26 +14,23 @@ public class Board {
         return board;
     }
 
-    public boolean place(int[] location, CellState player) {
-        if(board[location[1]][location[0]] == CellState.BLANK) {
-            board[location[1]][location[0]] = player;
-            return true;
-        } else {
-            return false;
-        }
+    public void place(Integer[] location, CellState player) {
+        board[location[0]][location[1]] = player;
     }
 
     public void printBoard() {
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[i].length; j++) {
-                if(board[i][j] != CellState.BLANK) {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(2 - i + " ");
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != CellState.BLANK) {
                     System.out.print(board[i][j] + (j + 1 == board[i].length ? "" : "|"));
                 } else {
                     System.out.print(" " + (j + 1 == board[i].length ? "" : "|"));
                 }
             }
-            System.out.print(i + 1 == board.length ? "" : "\n-----\n");
+            System.out.print(i + 1 == board.length ? "" : "\n  -----\n");
         }
+        System.out.println("\n  0 1 2");
         System.out.println();
     }
 
@@ -38,53 +38,74 @@ public class Board {
         CellState checkingPlayer;
 
         //check columns
-        for(int i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             checkingPlayer = board[0][i];
-            if(checkingPlayer == CellState.BLANK) {
+            if (checkingPlayer == CellState.BLANK) {
                 checkingPlayer = null;
                 continue;
             }
-            for(int j = 0; j < board[i].length; j++) {
-                if(board[j][i] != checkingPlayer) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[j][i] != checkingPlayer) {
                     checkingPlayer = null;
                     break;
                 }
             }
-            if(checkingPlayer != null) {
+            if (checkingPlayer != null) {
                 return checkingPlayer;
             }
         }
 
         //check rows
-        for(int i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             checkingPlayer = board[i][0];
-            if(checkingPlayer == CellState.BLANK) {
+            if (checkingPlayer == CellState.BLANK) {
                 checkingPlayer = null;
                 continue;
             }
-            for(int j = 0; j < board[i].length; j++) {
-                if(board[i][j] != checkingPlayer) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != checkingPlayer) {
                     checkingPlayer = null;
                     break;
                 }
             }
-            if(checkingPlayer != null) {
+            if (checkingPlayer != null) {
                 return checkingPlayer;
             }
         }
 
         //check diagonals
         checkingPlayer = board[0][0];
-        if(board[1][1] == checkingPlayer && board[2][2] == checkingPlayer && checkingPlayer != CellState.BLANK) {
+        if (board[1][1] == checkingPlayer && board[2][2] == checkingPlayer && checkingPlayer != CellState.BLANK) {
             return checkingPlayer;
         }
 
         checkingPlayer = board[2][0];
-        if(board[1][1] == checkingPlayer && board[0][2] == checkingPlayer && checkingPlayer != CellState.BLANK) {
+        if (board[1][1] == checkingPlayer && board[0][2] == checkingPlayer && checkingPlayer != CellState.BLANK) {
             return checkingPlayer;
         }
 
-        return null;
+        //check tie;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == CellState.BLANK) {
+                    return null;
+                }
+            }
+        }
+
+        return CellState.BLANK;
+    }
+
+    public List<Integer[]> getAvailablePositions() {
+        ArrayList<Integer[]> availablePositions = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == Board.CellState.BLANK) {
+                    availablePositions.add(new Integer[]{i, j});
+                }
+            }
+        }
+        return availablePositions;
     }
 
     public enum CellState {
