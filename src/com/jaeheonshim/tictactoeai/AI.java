0d93dadb6baed44867e012e05c73c9c8a55ff576
Jaeheon Shim <git@jaeheonshim.com>
2020-01-16 19:58:38 -0500

    Restrict placing piece on already placed piece, move main method to separate class, check for valid strings

--- src/com/jaeheonshim/tictactoeai/AI.java
+++ src/com/jaeheonshim/tictactoeai/AI.java
@@ -4,94 +4,61 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AI {
-    public static Board.CellState ai = Board.CellState.X;
-    public static Board.CellState human = Board.CellState.O;
+	 public static Board.CellState ai = Board.CellState.X;
+	 public static Board.CellState human = Board.CellState.O;
 
-    public static Integer[] getBestMove(Board board) {
-        Integer[] bestMove = null;
-        int bestScore = -100;
-        for (Integer[] position : board.getAvailablePositions()) {
-            board.place(position, ai);
-            int score = minimax(board, false);
-            board.place(position, Board.CellState.BLANK);
-            if (score > bestScore) {
-                bestScore = score;
-                bestMove = position;
-            }
-        }
-        return bestMove;
-    }
+	 public static Integer[] getBestMove (Board board) {
+		  Integer[] bestMove = null;
+		  int bestScore = -100;
+		  for (Integer[] position : board.getAvailablePositions()) {
+				board.place(position, ai);
+				int score = minimax(board, false);
+				board.place(position, Board.CellState.BLANK);
+				if (score > bestScore) {
+					 bestScore = score;
+					 bestMove = position;
+				}
+		  }
+		  return bestMove;
+	 }
 
-    public static int minimax(Board board, boolean maximising) {
-        Board.CellState winner = board.checkWon();
-        int score;
-        if (winner != null) {
-            if (winner == ai) {
-                score = 1;
-                return score;
-            } else if (winner == Board.CellState.BLANK) {
-                score = 0;
-                return score;
-            } else {
-                score = -1;
-                return score;
-            }
-        }
-        if (maximising) {
-            int bestScore = -100;
-            for (Integer[] position : board.getAvailablePositions()) {
-                board.place(position, ai);
-                int currentScore = minimax(board, false);
-                board.place(position, Board.CellState.BLANK);
-                if(currentScore > bestScore) {
-                    bestScore = currentScore;
-                }
-            }
-            return bestScore;
-        } else {
-            int bestScore = 100;
-            for (Integer[] position : board.getAvailablePositions()) {
-                board.place(position, human);
-                int currentScore = minimax(board, true);
-                board.place(position, Board.CellState.BLANK);
-                if(currentScore < bestScore) {
-                    bestScore = currentScore;
-                }
-            }
-            return bestScore;
-        }
-    }
-
-    public static void main(String[] args) {
-        Board board = new Board();
-        Scanner in = new Scanner(System.in);
-        board.printBoard();
-        while(board.checkWon() == null) {
-            System.out.print("Enter position (x, y): ");
-            String[] position = in.nextLine().split(",");
-            Integer[] pos = new Integer[2];
-            pos[0] = 2 - Integer.parseInt(position[1].trim());
-            pos[1] = Integer.parseInt(position[0].trim());
-            board.place(pos, Board.CellState.O);
-            board.printBoard();
-            System.out.println();
-            if(board.checkWon() != null) {
-                break;
-            }
-            System.out.println("AI is playing...");
-            board.place(getBestMove(board), Board.CellState.X);
-            board.printBoard();
-        }
-
-        System.out.println("=======================");
-        System.out.println("Game Finished!");
-        board.printBoard();
-        if(board.checkWon() == ai) {
-            System.out.println("AI WON!");
-        } else if(board.checkWon() == human) {
-            System.out.println("YOU WON?!");
-        } else {
-            System.out.println("TIE");
-        }
-    }
+	 public static int minimax (Board board, boolean maximising) {
+		  Board.CellState winner = board.checkWon();
+		  int score;
+		  if (winner != null) {
+				if (winner == ai) {
+					 score = 1;
+					 return score;
+				} else if (winner == Board.CellState.BLANK) {
+					 score = 0;
+					 return score;
+				} else {
+					 score = -1;
+					 return score;
+				}
+		  }
+		  if (maximising) {
+				int bestScore = -1;
+				for (Integer[] position : board.getAvailablePositions()) {
+					 board.place(position, ai);
+					 int currentScore = minimax(board, false);
+					 board.place(position, Board.CellState.BLANK);
+					 if (currentScore > bestScore) {
+						  bestScore = currentScore;
+					 }
+				}
+				return bestScore;
+		  } else {
+				int bestScore = 1; // calling it best score is confusing because we want this to be the lowest score to see the worst possible scenario
+				for (Integer[] position : board.getAvailablePositions()) {
+					 board.place(position, human);
+					 int currentScore = minimax(board, true);
+					 board.place(position, Board.CellState.BLANK);
+					 if (currentScore < bestScore) {
+						  bestScore = currentScore;
+					 }
+				}
+				return bestScore;
+		  }
+	 }
 }
---
+++ src/com/jaeheonshim/tictactoeai/Main.java
@@ -0,0 +1,58 @@
+package com.jaeheonshim.tictactoeai;
+
+import java.util.Scanner;
+
+import static com.jaeheonshim.tictactoeai.AI.*;
+
+public class Main {
+	 public static void main (String[] args) {
+		  Board board = new Board();
+		  Scanner in = new Scanner(System.in);
+		  board.printBoard();
+		  while (board.checkWon() == null) {
+				Integer[] pos;
+				boolean inputValid = true;
+				do {
+					 System.out.print("Enter position (x, y): ");
+					 inputValid = true;
+					 String[] position = in.nextLine().split(",");
+					 pos = new Integer[2];
+					 if(position.length < 2) {
+					 	 inputValid = false;
+					 	 continue;
+					 }
+					 try {
+						  pos[0] = 2 - Integer.parseInt(position[1].trim());
+						  pos[1] = Integer.parseInt(position[0].trim());
+					 } catch (NumberFormatException e) {
+					 	 inputValid = false;
+					 }
+					 if(board.getBoard()[pos[0]][pos[1]] != Board.CellState.BLANK) {
+					 	 inputValid = false;
+					 }
+				} while(!inputValid);
+
+				board.place(pos, Board.CellState.O);
+
+				board.printBoard();
+				System.out.println();
+				if (board.checkWon() != null) {
+					 break;
+				}
+				System.out.println("AI is playing...");
+				board.place(getBestMove(board), Board.CellState.X);
+				board.printBoard();
+		  }
+
+		  System.out.println("=======================");
+		  System.out.println("Game Finished!");
+		  board.printBoard();
+		  if (board.checkWon() == ai) {
+				System.out.println("AI WON!");
+		  } else if (board.checkWon() == human) {
+				System.out.println("YOU WON?!");
+		  } else {
+				System.out.println("TIE");
+		  }
+	 }
+}
